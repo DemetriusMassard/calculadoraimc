cfa327fc85e90687e54000cbf50a1155af4de502
Demetrius Massard <demetrius2597@hotmail.com>
2025-04-29 20:58:23 -0300

    Initial commit Cleanup

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:calculadoraimc/app/home/homepage.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(colorScheme: ColorScheme.dark(), useMaterial3: true),
+      home: const HomePage(),
+    );
+  }
+}
